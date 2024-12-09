d3ffd97849c8bc34f9b037bf3e1b9ec67fb3d1b8
Carlos Godoy <carlosgodoy@pursuit.org>
2024-12-09 12:08:08 -0500

    adding recursion / binary tree notes

--- recursion.js
+++ recursion.js
@@ -1,13 +1,43 @@
 // Recursion: occurs when a function invokes itself. These functions need a base case in order to exit the function when a certain condition has been met
 
+// A recursive has two CASES:
+// - 1: a base case that will allow us to exit the function and return a value
+// - 2: a recursive case that invokes the function itself with arguments that move us closer to the base case
+
 
 // Add up all the numbers between 1 and num
+// const recursiveAddition = (num) => {
+//     // base case
+//     if(num === 1){
+//         return 1
+//     }
+//     return num + recursiveAddition(num - 1)
+// }
+
+// console.log(recursiveAddition(3))
+
+
+// Write a recursive that takes in a number and adds up all of the numbers between 1 and the number provided
 const recursiveAddition = (num) => {
-    // base case
-    if(num === 1){
-        return 1
+    // negative number guard clause
+    if(num < 0){
+        return "Number must be positive"
     }
+    // base case
+    if(num === 1 || num === 0){
+        return num
+    }
+    // recursive case
     return num + recursiveAddition(num - 1)
 }
 
-console.log(recursiveAddition(3))
+// recursiveAddition(0) => 0
+// recursiveAddition(1) => 1
+// recursiveAddition(2) => 2 + recursiveAddition(1) => 2 + 1 => 3
+// recursiveAddition(3) => 3 + recursiveAddition(2) => 3 + 3 => 6
+// recursiveAddition(4) => 4 + recursiveAddition(3) => 4 + 6 => 10
+// recursiveAddition(5) => 5 + recursiveAddition(4) => 5 + 10 => 15
+
+// 5 + 4 + 3 + 2 + 1
+// console.log(recursiveAddition(5))
+
--- trees.js
+++ trees.js
@@ -13,15 +13,44 @@ class TreeNode {
     }
 }
 
-
+// Difference between Binary tree and Binary Search Tree is that BSTs are in order
 class BinarySearchTree {
     constructor(root=null){
         this.root = root
     }
 
-    insert(){}
+    insert(data){
+        const newNode = new TreeNode(data)
+        // if there is no root node, then assign this.root to be newNode
+        if(!this.root){
+            this.root = newNode
+        } else {
+            // find a home for newNode
+            this.recursiveInsert(this.root, newNode)
+        }
+    }
 
-    recursiveInsert(){}
+    recursiveInsert(node, newNode){
+        // compare the node's value to the newNode's value
+        if(newNode.data < node.data){
+            // check if there's a node on the left
+            if(node.left){
+                // if there is a node there, keep traversing to find a home for newNode
+                this.recursiveInsert(node.left, newNode)
+            } else {
+                 // since there is no node on the left, we assign newNode to be the left pointer to the node
+                node.left = newNode
+            }
+        } else {
+            if(node.right){
+                // if there is a node on the right hand side, keep traversing to find a home for the newNode
+                this.recursiveInsert(node.right, newNode)
+            } else {
+                // since there is no node on the right, we assign newNode to be the right pointer to the node
+                node.right = newNode
+            }
+        }
+    }
 
     // Breadth-first traversal where add up all the vals (using an array queue)
     addVals(){
@@ -71,9 +100,19 @@ class BinarySearchTree {
         }
         return total
     }
+    // Add up all the nodes' data using recursion
+    addValsRecursively(node=this.root){
+        // base case
+        if(!node){
+            return 0
+        }
+        return node.data + this.addValsRecursively(node.left) + this.addValsRecursively(node.right)
+    }
 }
+
 const { inspect } = require('util')
 const tree = new BinarySearchTree()
+
 
 //        5
 //       / \
@@ -85,24 +124,38 @@ const tree = new BinarySearchTree()
 
 // order of nodes gettings hit in breadth first: 5371492
 
-const five = new TreeNode(5)
-const three = new TreeNode(3)
-const one = new TreeNode(1)
-const two = new TreeNode(2)
-const four = new TreeNode(4)
-const seven = new TreeNode(7)
-const nine = new TreeNode(9)
+// const five = new TreeNode(5)
+// const three = new TreeNode(3)
+// const one = new TreeNode(1)
+// const two = new TreeNode(2)
+// const four = new TreeNode(4)
+// const seven = new TreeNode(7)
+// const nine = new TreeNode(9)
 
-tree.root = five
+// tree.root = five
 
-five.left = three
-five.right = seven
-seven.right = nine
-three.left = one
-three.right = four
-one.right = two
+// five.left = three
+// five.right = seven
+// seven.right = nine
+// three.left = one
+// three.right = four
+// one.right = two
 
 // console.log(inspect(tree, true, 10, true))
 
-console.log(tree.addVals())
-console.log(tree.addVals2())
+
+
+tree.insert(5)
+tree.insert(3)
+tree.insert(7)
+tree.insert(1)
+tree.insert(4)
+tree.insert(2)
+tree.insert(9)
+
+// console.log(inspect(tree, true, 10, true))
+// console.log(tree.addVals())
+// console.log('-----------------')
+// console.log(tree.addVals2())
+
+console.log(tree.addValsRecursively())
